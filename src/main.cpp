de0e3fd97910a0f5c14dc7609a7f77af17ffc859
Emeric <tognonemeric@gmail.com>
2025-08-26 12:23:58 +0100

    Streaming fonctionnel et amélioration de la reconnexion wifi

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <Firebase_ESP_Client.h>
 #include "time_utils.h"
 #include "secrets.h" 
+#include "certs.h"
+
 // Bibliothèque auxiliaire pour le jeton d'authentification
 #include "addons/TokenHelper.h"  
 #include "addons/RTDBHelper.h" 
@@ -13,7 +15,10 @@
 #define SOIL_MOISTURE_PIN A0
 
 
+// Brochage des actionneurs et état
 
+#define RELAY_PIN 14 // D5
+bool RELAY_STATE = false;
 // Objet DHT 
 DHT dht(DHT_PIN,DHT_TYPE);
 
@@ -26,11 +31,14 @@ FirebaseConfig config;
 void initializeTimestamp();
 void streamCallback(FirebaseStream data);
 void streamTimeoutCallback(bool timeout);
-
+void getInitialRelayState();
+void checkWiFiConnection();
 // setup
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH); // Fermé par défaut
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connexion au WiFi");
@@ -44,6 +52,7 @@ void setup() {
    // Configuration Firebase
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
+  config.cert.data = FIREBASE_ROOT_CA;
 
    // Auth Email/Password
    auth.user.email = USER_EMAIL;
@@ -51,28 +60,44 @@ void setup() {
   
   // Initialisation Firebase
   Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-  //Démarrage de l'écoute 
-  if(!Firebase.RTDB.beginStream(&fbdo,"cultures/dernieres_mesures")){
-    Serial.println("Echec stream : " + fbdo.errorReason());
+  Firebase.reconnectNetwork(true);
+
+  // Attendre que Firebase soit prêt
+  int attempts = 0;
+  while (!Firebase.ready() && attempts < 10) {
+    delay(1000);
+    attempts++;
+    Serial.print(".");
   }
-  // Initialisation de l'écoute avec les callbacks 
-  Firebase.RTDB.setStreamCallback(&fbdo,streamCallback,streamTimeoutCallback);
-  // Initialisation du serveur NTP
+  if(Firebase.ready()) {
+    getInitialRelayState();
+    
+    Serial.println("Démarrage de l'écoute");
+    if(!Firebase.RTDB.beginStream(&fbdo,"cultures/dernieres_mesures/etat_electrovanne")){
+      Serial.println("Echec stream : " + fbdo.errorReason());
+    }
+    Firebase.RTDB.setStreamCallback(&fbdo,streamCallback,streamTimeoutCallback);
+  } else {
+    Serial.println("Échec de la connexion Firebase" + fbdo.errorReason());
+  }
+
   initializeTimestamp();
 
 }
 
 void loop() {
+  checkWiFiConnection();  // Vérifier WiFi régulièrement
+  
   if(Firebase.ready()){
     Serial.println("Firebase prêt !");
     String ts = getIsoUtcTime();
     Serial.println(ts);
-    delay(5000);
   }
   else {
     Serial.println("Firebase non prêt !");
+    Serial.printf("Mémoire libre: %d bytes\n", ESP.getFreeHeap());
   }
+  delay(5000);
 }
 
 void initializeTimestamp() {
@@ -81,13 +106,25 @@ void initializeTimestamp() {
 }
 
 void streamCallback(FirebaseStream data) {
-  Serial.println("Données reçues depuis Firebase !");
-  Serial.print("Chemin écouté :");
+  Serial.println("\n=== Données reçues depuis Firebase ===");
+  Serial.print("Chemin écouté complet: ");
   Serial.println(data.streamPath());
-  Serial.print("Valeur : ");
+  Serial.print("Type de donnée: ");
+  Serial.println(data.dataType());
+  Serial.print("Valeur: ");
   Serial.println(data.stringData());
-  Serial.print("Chemin de la donnée changée : ");
-  Serial.println(data.dataPath());
+  
+  // Maintenant on reçoit directement la valeur booléenne
+  if(data.dataType() == "boolean"){
+    RELAY_STATE = data.boolData();
+    digitalWrite(RELAY_PIN, RELAY_STATE ? LOW : HIGH);
+    Serial.print("Nouvel état de l'électrovanne : ");
+    Serial.println(RELAY_STATE ? "OUVERT" : "FERME");
+    Serial.println("=== Changement appliqué ===");
+  }
+  else {
+    Serial.println("=== Type de donnée inattendu ===");
+  }
 }
 
 void streamTimeoutCallback(bool timeout) {
@@ -96,5 +133,39 @@ void streamTimeoutCallback(bool timeout) {
   }
   if(!fbdo.httpConnected()) {
     Serial.println("Erreur : " + fbdo.errorReason());
+  }
+}
+
+void getInitialRelayState(){
+  if(Firebase.RTDB.getBool(&fbdo,"cultures/dernieres_mesures/etat_electrovanne")){
+    if(fbdo.dataType() == "boolean"){
+      bool initial_state = fbdo.boolData();
+      RELAY_STATE = initial_state;
+      digitalWrite(RELAY_PIN, RELAY_STATE ? LOW : HIGH);
+      Serial.print("Etat initial de l'électrovanne : ");
+      Serial.println(RELAY_STATE ? "OUVERT" : "FERME");
+      Serial.print("Valeur pin: ");
+      int state = digitalRead(RELAY_PIN);
+      Serial.println(state);
+      Serial.print("Etat : ");
+      Serial.println(state == LOW ? "OUVERT" : "FERME");
+    }else{
+      Serial.print("Type de donnée inconnue: ");
+      Serial.println(fbdo.dataType());
+      Serial.print("Valeur reçue: ");
+      Serial.println(fbdo.stringData());
+    }
+  }else{
+    Serial.print("Erreur lors de la récupération: ");
+    Serial.println(fbdo.errorReason());
+  }
+}
+
+void checkWiFiConnection() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi perdu, tentative de reconnexion...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    delay(3000);
   }
 }
